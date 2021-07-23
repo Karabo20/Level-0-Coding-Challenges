b30b0dc1290ba39a54f8438dd1d24dae715c6ad1
Karabo <mrkaymp@gmail.com>
2021-07-23 17:14:02 +0200

    Task 8, code modified to consider singular and plural

--- task_0.8.py
+++ task_0.8.py
@@ -1,9 +1,14 @@
 # Task 0.8
-
 def convert_time(minutes):
     hours = 0
     while minutes > 60:
         minutes = minutes - 60
         hours = hours + 1
-    print(str(hours)+ " hour, " +str(minutes)+ " minutes ")
-convert_time(170)
+    if hours <= 1 and minutes <= 1:
+        print(str(hours)+ " hour, " +str(minutes)+ " minute")
+    elif hours > 1 and minutes <= 1:
+        print(str(hours)+ " hours, " +str(minutes)+ " minute")
+    elif hours <= 1 and minutes > 1:
+        print(str(hours)+ " hour, " +str(minutes)+ " minutes")
+    else:
+        print(str(hours)+ " hours, " +str(minutes)+ " minutes ")
