3b12042f8ef80bc0e234960481dc34e398576710
Karabo <mrkaymp@gmail.com>
2021-07-23 17:18:42 +0200

    Task 10, code modified to mimick example

--- task_0.10.py
+++ task_0.10.py
@@ -1,7 +1,9 @@
 # Task 0.10
-
 def common(phrase1, phrase2):
+    com_letters = []
+    print("Common letters: ", end="")
     for i in phrase1:
         if i in phrase2:
-            print(i)    
+            com_letters.append(i)
+    print(",".join(com_letters))
 common("computers", "house")
