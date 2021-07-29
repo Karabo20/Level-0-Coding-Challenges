a2d65f07ef2ae788abb87a13aa6979c2ddfb5551
Karabo20 <62095778+Karabo20@users.noreply.github.com>
2021-07-29 17:40:46 +0200

    Code caters capital and small letters

--- task_0.9.py
+++ task_0.9.py
@@ -1,11 +1,10 @@
-#Task 0.9
+#Task0.9
 def vowel_only(keyword):
     vowels = [] 
-    print("Vowels: ", end="")
-    for i in keyword:
-        if i.islower() and i in "aeiou":
-            vowels.append(i)
+    for letter in keyword:
+        if letter in "AEIOU" or letter in "aeiou":
+            vowels.append(letter)
         else:
             continue
-    print(",".join(vowels))
+    print("Vowels:", ",".join(vowels))
 vowel_only("Umuzi")
