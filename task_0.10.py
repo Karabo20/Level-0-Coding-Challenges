6ed5885ba4e091206632f130ab8af9d3c74d0e63
Karabo20 <62095778+Karabo20@users.noreply.github.com>
2021-07-29 17:25:19 +0200

    Modified code, task_010.py

--- task_0.10.py
+++ task_0.10.py
@@ -1,9 +1,7 @@
 # Task 0.10
 def common(phrase1, phrase2):
-    com_letters = []
-    print("Common letters: ", end="")
-    for i in phrase1:
-        if i in phrase2:
-            com_letters.append(i)
-    print(",".join(com_letters))
-common("computers", "house")
+    common_letters = []
+    for letter in phrase1:
+        if letter in phrase2:
+            common_letters.append(letter)
+    print("Common letters:", ",".join(common_letters))
