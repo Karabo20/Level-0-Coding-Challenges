e8adfdc6626549599bfed04b469c074ccbe26606
Karabo20 <62095778+Karabo20@users.noreply.github.com>
2021-07-29 17:49:59 +0200

    Code returns area

--- task_0.5.py
+++ task_0.5.py
@@ -1,5 +1,5 @@
 # Task 0.5
 def triangle(side1, side2, side3):
-    area = 1/2 * (side1 + side2 + side3)
+    semi_p = 0.5 * (side1 + side2 + side3)                                    
+    area = (semi_p * (semi_p - side1)*(semi_p - side2)*(semi_p - side3)) ** 0.5
     return area
-triangle(3,4,5)
